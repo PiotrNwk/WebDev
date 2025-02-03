e3bf022810a5b1a2231c2f9e38487de289193829
PiotrNwk <146440665+PiotrNwk@users.noreply.github.com>
2025-02-03 20:30:06 +0100

    Update jQueryAjax.js

--- jQueryAjax.js
+++ jQueryAjax.js
@@ -1,4 +1,8 @@
- let statusCol = $(".table_matrix_td0, .table_matrix_td1").filter(function () {
-                                            let text = $(this).text().trim();
-                                            return statusList.includes(text);
-                                        });
+let textValues = $(".table_matrix_td0, .table_matrix_td1").filter(function () {
+    let text = $(this).text().trim();
+    return statusList.includes(text);
+}).map(function() {
+    return $(this).text().trim();
+}).get();
+
+console.log(textValues);
