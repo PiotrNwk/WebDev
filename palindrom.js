66c453891ac877c454931f6aab94683dc8d8256a
PiotrNwk <146440665+PiotrNwk@users.noreply.github.com>
2025-02-16 23:28:36 +0100

    Update palindrom.js

--- palindrom.js
+++ palindrom.js
@@ -1,1 +1,15 @@
 // Palindrom 16/02/2025
+
+function isPalindrome(str) {
+    // Usuwamy spacje i konwertujemy na małe litery
+    str = str.toLowerCase().replace(/\s/g, '');
+    
+    // Porównujemy ciąg znaków z jego odwróconą wersją
+    return str === str.split('').reverse().join('');
+}
+
+// Testy
+console.log(isPalindrome("radar")); // true
+console.log(isPalindrome("Kajak")); // true
+console.log(isPalindrome("hello")); // false
+console.log(isPalindrome("A man a plan a canal Panama")); // true
